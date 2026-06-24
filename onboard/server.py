4770173b978122e7befef0eef6577bdb5c088f0d
Alexhellupov-cyber <alexhell132@gmail.com>
2026-06-24 09:43:07 +0300

    DroneOS Alpha 0.7.5

--- onboard/server.py
+++ onboard/server.py
@@ -19,6 +19,12 @@ class OnboardServer:
             socket.SOCK_STREAM
         )
 
+        self.server.setsockopt(
+            socket.SOL_SOCKET,
+            socket.SO_REUSEADDR,
+            1
+        )
+
         self.server.bind((HOST, PORT))
         self.server.listen(1)
 

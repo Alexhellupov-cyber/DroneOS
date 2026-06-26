654ac9c457f180e2461d4e27cade354d6b1431b3
Alexhellupov-cyber <alexhell132@gmail.com>
2026-06-26 13:08:14 +0300

    DroneOS Alpha 0.8.7.2

--- src/video/video_thread.py
+++ src/video/video_thread.py
@@ -2,6 +2,8 @@ from PySide6.QtCore import QThread, Signal
 from PySide6.QtGui import QImage
 
 import cv2
+import numpy as np
+import requests
 
 
 class VideoThread(QThread):
@@ -12,38 +14,53 @@ class VideoThread(QThread):
         super().__init__()
 
         self.running = True
-
         self.url = f"http://{ip}:5000/video"
 
     def run(self):
 
-        cap = cv2.VideoCapture(self.url)
+        stream = requests.get(self.url, stream=True)
 
-        while self.running:
+        buffer = bytes()
 
-            ok, frame = cap.read()
+        for chunk in stream.iter_content(chunk_size=4096):
 
-            if not ok:
-                continue
+            if not self.running:
+                break
 
-            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            buffer += chunk
 
-            h, w, ch = rgb.shape
+            a = buffer.find(b'\xff\xd8')
+            b = buffer.find(b'\xff\xd9')
 
-            image = QImage(
-                rgb.data,
-                w,
-                h,
-                ch * w,
-                QImage.Format_RGB888,
-            ).copy()
+            if a != -1 and b != -1:
 
-            self.frame_ready.emit(image)
+                jpg = buffer[a:b+2]
 
-        cap.release()
+                buffer = buffer[b+2:]
+
+                frame = cv2.imdecode(
+                    np.frombuffer(jpg, dtype=np.uint8),
+                    cv2.IMREAD_COLOR
+                )
+
+                if frame is None:
+                    continue
+
+                rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                h, w, ch = rgb.shape
+
+                image = QImage(
+                    rgb.data,
+                    w,
+                    h,
+                    ch * w,
+                    QImage.Format_RGB888
+                ).copy()
+
+                self.frame_ready.emit(image)
 
     def stop(self):
 
         self.running = False
-
         self.wait()

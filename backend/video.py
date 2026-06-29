ccbc1c03acf6873d4a383c4492a6849f41b339c0
Alexhellupov-cyber <alexhell132@gmail.com>
2026-06-29 10:01:04 +0300

    DroneOS Alpha 0.9.2.1

--- backend/video.py
+++ backend/video.py
@@ -13,3 +13,17 @@ class VideoCamera:
 
         self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    def get_frame(self):
+        success, frame = self.camera.read()
+
+        if not success:
+            return None
+
+        _, buffer = cv2.imencode(".jpg", frame)
+
+        return buffer.tobytes()
+
+    def __del__(self):
+        if self.camera.isOpened():
+            self.camera.release()
